6cf1da9746eb4444ad7aecb0d909e9565812d471
arthursl12 <arthursoutolima@gmail.com>
2020-11-05 12:39:15 -0300

    Última verificação

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Formato: pypy3 main.py [entrada] [saída]
-# pypy3 main.py CG_examples/test5.in out1.ppm
+pypy3 main.py CG_examples/test5.in out1.ppm
 
 # Formato: pypy3 main.py [entrada] [saída] [-t [largura] [altura]] [-a samplesAA]
-pypy3 main.py examples/gif1.in examples/gif1.gif -t 1280 720
+# pypy3 main.py examples/gif1.in examples/gif1.gif -t 1280 720
